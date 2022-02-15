de3e5c45a1700a6cf1a4fb988911d9b03f63dfeb
benjenkinsv95 <benjenkinsv95@gmail.com>
2022-02-15 17:44:35 -0600

    Add emphasis to about me sections

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -19,15 +19,15 @@ const About = () => (
       <div className='col-sm-8'>
         <h2 className='title-h2 text-secondary'>About Me</h2>
         <Paragraph>
-            I{'\''}m a software engineering instructor at General Assembly. Each day I have the opportunity to teach new developers the skills they need to land jobs as full-stack developers.
+            I{'\''}m a software engineering instructor at General Assembly. I have had the opportunity to <span className='text-secondary'>teach over 500 new developers</span> in demand skills, such as <span className='text-primary'>React, Express, & Django</span> that they used to land jobs as full-stack developers.
         </Paragraph>
 
         <Paragraph>
-            Prior to General Assembly I developed software for humanoid robots which delivered therapy lessons to children with Autism.
+            Prior to General Assembly I developed software for <span className='text-primary'>humanoid robots</span> which delivered therapy lessons to children with Autism.
         </Paragraph>
 
         <Paragraph>
-            Alongside work, I completed a Masters in Software Engineering from Harvard Extension School and a BA in Computer Science from TESU.
+            Alongside work, I completed a <span className='text-secondary'>Masters in Software Engineering</span> from <span className='text-primary'>Harvard Extension School</span> and a BA in Computer Science from TESU.
         </Paragraph>
         <div>
           <a href='mailto:benjenkinsv95@gmail.com' target='_blank' rel="noreferrer">
