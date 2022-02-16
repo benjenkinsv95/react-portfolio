585e087dfada5a2c25ee7f4cfdf7ebf44185c9c9
benjenkinsv95 <benjenkinsv95@gmail.com>
2022-02-15 23:40:07 -0600

    Make about buttons more responsive

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import profilePic from './profile_pic.jpg'
 import resume from './ben-jenkins-resume.pdf'
 import styled from 'styled-components'
+import useWindowSize from '../../lib/use-window-size'
 
 const Paragraph = styled.p`
     font-size: 1.3em;
@@ -13,40 +14,43 @@ const Button = styled.button`
     font-weight: 600;
 `
 
-const About = () => (
-  <div id='about' className='container'>
-    <div className='row mb-4'>
-      <div className='col-sm-8 mt-4'>
-        <h2 className='title-h2 text-secondary'>About Me</h2>
-        <Paragraph>
+const About = () => {
+  const { width } = useWindowSize()
+  return (
+    <div id='about' className='container'>
+      <div className='row mb-4'>
+        <div className='col-sm-8 mt-4'>
+          <h2 className='title-h2 text-secondary'>About Me</h2>
+          <Paragraph>
             I{'\''}m a software engineering instructor at General Assembly. I have had the opportunity to <span className='text-secondary'>teach over 500 new developers</span> in demand skills, such as <span className='text-primary'>React, Express, & Django</span> that they used to land jobs as full-stack developers.
-        </Paragraph>
+          </Paragraph>
 
-        <Paragraph>
+          <Paragraph>
             Prior to General Assembly I developed software for <span className='text-primary'>humanoid robots</span> which delivered therapy lessons to children with Autism.
-        </Paragraph>
+          </Paragraph>
 
-        <Paragraph>
+          <Paragraph>
             Alongside work, I completed a <span className='text-secondary'>Masters in Software Engineering</span> from <span className='text-primary'>Harvard Extension School</span> and a BA in Computer Science from TESU.
-        </Paragraph>
-        <div>
-          <a href='mailto:benjenkinsv95@gmail.com' target='_blank' rel="noreferrer">
-            <Button className='btn btn-secondary btn-lg me-3'>
+          </Paragraph>
+          <div>
+            <a href='mailto:benjenkinsv95@gmail.com' target='_blank' rel="noreferrer">
+              <Button className={`btn btn-secondary ${width > 400 ? 'btn-lg' : ''} me-3`}>
               Get in Touch
-            </Button>
-          </a>
-          <a href={resume} target='_blank' rel="noreferrer">
-            <Button className='btn btn-outline-primary btn-lg'>
+              </Button>
+            </a>
+            <a href={resume} target='_blank' rel="noreferrer">
+              <Button className={`btn btn-outline-primary ${width > 400 ? 'btn-lg' : ''}`}>
               Download Resume
-            </Button>
-          </a>
+              </Button>
+            </a>
+          </div>
+        </div>
+        <div className='col-sm-4 mt-4'>
+          <img className='img-fluid' src={profilePic} />
         </div>
       </div>
-      <div className='col-sm-4 mt-4'>
-        <img className='img-fluid' src={profilePic} />
-      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default About
